8bcf17d792247c76586007355d8e0fca2c650295
Manik Maity <manikmaity156@gmail.com>
2024-08-16 11:44:30 +0530

    Added navbar, banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css'
+import Banner from './Components/Banner/Banner'
 import CoinTable from './Components/CoinTable/CoinTable'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
+    <Navbar/>
+    <Banner/>
      <CoinTable/>
     </>
   )
--- src/Components/CoinTable/CoinTable.jsx
+++ src/Components/CoinTable/CoinTable.jsx
@@ -1,32 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
-function CoinTable() {
-
-    let [count, setCount] = useState(1);
-
-    async function download (){
-        const jsonResponse = await fetch("https://fakestoreapi.com/products");
-        const data = await jsonResponse.json();
-        console.log(data);
-    }
-
-    useEffect(() => {
-        download();
-    }, []) // execute one  time in mounting
-
-    useEffect(() => {
-        console.log("Change everytime")
-    }) // change on all re-render because dependency array is not passed
-
-    useEffect(() => {
-        console.log("count changed")
-    }, [count])
-
-
+const CoinTable = () => {
   return (
     <div>
-      <p>Coin Table {count}</p>
-      <button onClick={() => {setCount(++count)}}>Increase</button>
+      <h1>Coin Table</h1>
     </div>
   )
 }
